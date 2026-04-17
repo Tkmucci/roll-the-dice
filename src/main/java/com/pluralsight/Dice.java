8ec974977cbdcb69add16c3fe747d8f6b01dff60
takudzwa mucherengi <takudzwamucherengi@icloud.com>
2026-04-17 14:35:37 -0500

    2nd Commit: Added all the necessary comments and cleaned it out

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -1,11 +1,15 @@
 package com.pluralsight;
 
+//Dice Class
 public class Dice {
 
+    //Main Method
     public static void main(String[] args) {
 
+        //an instance of Dice named dice
         Dice dice = new Dice();
 
+        //Declaration of my variables
         int counterTwo = 0;
 
         int counterFour = 0;
@@ -14,17 +18,19 @@ public class Dice {
 
         int counterSeven = 0;
 
-        int sum = 0;
+        int sum;
+
+        int i;
 
 
-//        roll();
-        int i = 0;
-
+        //a loop that executes a hundred times
         for (i = 1; i < 100; i++) {
 
+            //integer variables for roll1 and roll2
             int roll1 = dice.roll();
             int roll2 = dice.roll();
 
+            //sum that adds the two rolls together
             sum = roll1 + roll2;
 
             System.out.println("Roll 1: " + roll1 + " + " + roll2 + " Sum: " + sum);
@@ -32,6 +38,7 @@ public class Dice {
 //        System.out.println("Roll 2: " + roll2);
 
 
+            //if/else statement that
             if (sum == 2) {
 
 
@@ -42,14 +49,14 @@ public class Dice {
 
 
                 counterFour++;
- //               System.out.println("Counter Four: " + counterFour);
+                //               System.out.println("Counter Four: " + counterFour);
 
 
             } else if (sum == 6) {
 
 
                 counterSix++;
- //               System.out.println("Counter Six: " + counterSix);
+                //               System.out.println("Counter Six: " + counterSix);
 
             } else if (sum == 7) {
 
@@ -61,21 +68,21 @@ public class Dice {
 
 
         }
+        //Print function that prints out the count
         System.out.printf("""
-                    \n
-                    Total rolls: %d
-                    Counter Two: %d
-                    Counter Four: %d
-                    Counter Six: %d
-                    Counter Seven: %d
-                    """, i, counterTwo, counterFour, counterSix, counterSeven);
+                \n
+                Total rolls: %d
+                Counter Two: %d
+                Counter Four: %d
+                Counter Six: %d
+                Counter Seven: %d
+                """, i, counterTwo, counterFour, counterSix, counterSeven);
     }
 
 
+    //roll method that generates a random number between 1 and 6
     public static int roll() {
 
-        int diceroll = (int) (Math.random() * 6) + 1;
-
-        return diceroll;
+        return (int) (Math.random() * 6) + 1;
     }
 }
